faa11284b18f90f6098e5216167be6d1d2fc37d2
Daisy <gos434@onet.eu>
2026-03-15 18:01:36 +0100

    Dodanie tabeli healthMetrics

---
+++ src/main/java/pl/wsb/fitnesstracker/healthmetrics/api/healthmetrics.java
@@ -0,0 +1,55 @@
+package pl.wsb.fitnesstracker.healthmetrics.api;
+
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import pl.wsb.fitnesstracker.user.api.User;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+@Entity
+@Table(name = "healthmetrics")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
+public class healthmetrics {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Nullable
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(name = "date", nullable = false)
+    private Date date;
+
+    @Column(name = "weight", nullable = false)
+    private double weight;
+
+    @Column(name = "height", nullable = false)
+    private double height;
+
+    @Column(name = "heartRate", nullable = false)
+    private double heartRate;
+
+
+    public healthmetrics(
+            final User user, final Date date, final double weight, final double height, final double heartRate) {
+
+        this.user = user;
+        this.date = date;
+        this.weight = weight;
+        this.height = height;
+        this.heartRate = heartRate;
+
+    }
+
+}
+
